dc434d0fd28b0da43d21202a12a55b13d32a2131
Kartik Chinda <78233115+KartikChinda@users.noreply.github.com>
2022-03-01 11:35:18 +0530

    Adding the backend part of the website

---
+++ Backend/index.js
@@ -0,0 +1,26 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+// using mongoDB
+const ConnURL = 'mongodb+srv://kartik11:1517sec4@cluster0.zkeb3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(ConnURL, { useNewUrlParser: true})
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
